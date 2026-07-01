3b57c5cff40e20deb2573f9c3fc8354b4edcabfb
Shiva Polaki <Shivprasadpolaki@gmail.com>
2026-07-01 20:55:01 +0530

    updated code to work in both vm's [headless] and locally

--- src/main/java/com/example/automation/base/LocalExecution.java
+++ src/main/java/com/example/automation/base/LocalExecution.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 public class LocalExecution implements TestExecutionStrategy {
 
 	@Override
-	public WebDriver setupDriver(String browserName) {
+/*	public WebDriver setupDriver(String browserName) {
 		// TODO Auto-generated method stub
 		switch (browserName.toLowerCase()) {
 		case "chrome":
@@ -28,7 +28,53 @@ public class LocalExecution implements TestExecutionStrategy {
 			edgeOptions.addArguments("--inprivate");
 			return new EdgeDriver(edgeOptions);
 		default:
-			return null;
-		}
+			return null;*/
+	
+	public WebDriver setupDriver(String browserName) {
+
+	    boolean isGitHub = System.getenv("GITHUB_ACTIONS") != null;
+
+	    switch (browserName.toLowerCase()) {
+
+	    case "chrome":
+	        ChromeOptions chromeOptions = new ChromeOptions();
+	        chromeOptions.addArguments("--incognito");
+
+	        if (isGitHub) {
+	            chromeOptions.addArguments("--headless=new");
+	            chromeOptions.addArguments("--no-sandbox");
+	            chromeOptions.addArguments("--disable-dev-shm-usage");
+	            chromeOptions.addArguments("--window-size=1920,1080");
+	        }
+
+	        return new ChromeDriver(chromeOptions);
+
+	    case "firefox":
+	        FirefoxOptions firefoxOptions = new FirefoxOptions();
+	        firefoxOptions.addArguments("-private");
+
+	        if (isGitHub) {
+	            firefoxOptions.addArguments("--headless");
+	        }
+
+	        return new FirefoxDriver(firefoxOptions);
+
+	    case "edge":
+	        EdgeOptions edgeOptions = new EdgeOptions();
+	        edgeOptions.addArguments("--inprivate");
+
+	        if (isGitHub) {
+	            edgeOptions.addArguments("--headless=new");
+	            edgeOptions.addArguments("--no-sandbox");
+	            edgeOptions.addArguments("--disable-dev-shm-usage");
+	            edgeOptions.addArguments("--window-size=1920,1080");
+	        }
+
+	        return new EdgeDriver(edgeOptions);
+
+	    default:
+	        throw new IllegalArgumentException(
+	                "Unsupported browser: " + browserName);
+	    }
 	}
 }
